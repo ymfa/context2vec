e67e9cb70f70c3c4cf6a96243002541d3e5850e6
qianchu <hey_flora@126.com>
2018-04-29 11:07:41 +0000

    env setup clean

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #run inside the docker nvidia-docker run  --name context2vec-gpu -it -p 8888:8888 -v /home/ql261/simp2trad/:/home/simp2trad/ chainer/chainer:v4.0.0-python2-lqc /bin/bash 
 
 #install optional
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install git
-git config --global user.email "hey_flora@126.com"
-git config --global user.name "qianchu"
+# sudo apt-get update
+# sudo apt-get upgrade
+# sudo apt-get install git
+# git config --global user.email "hey_flora@126.com"
+# git config --global user.name "qianchu"
 
 # set up bash shell
 # pack python project
@@ -16,9 +16,9 @@ sudo python setup.py install
 cd /home/
 sudo chmod -R 777 ./*
 cd /home/simp2trad/
-sudo python -m pip install --upgrade pip
-sudo python -m pip install jupyter
-sudo python -m pip install pandas
+# sudo python -m pip install --upgrade pip
+# sudo python -m pip install jupyter
+# sudo python -m pip install pandas
 
 if [ ! -d '/root/.jupyter/' ]; then
     sudo mkdir /root/.jupyter/
