03749b11db1604becaea4a04bfe923892f060c3a
qianchu <hey_flora@126.com>
2018-07-20 20:40:33 +0000

    batch test

--- context2vec/eval/hanzi_conversion.py
+++ context2vec/eval/hanzi_conversion.py
@@ -8,6 +8,7 @@ from collections import Counter, defaultdict
 from context2vec.common.model_reader import ModelReader
 import cupy as cp
 from chainer import cuda
+import math
 
 import re
 
@@ -30,11 +31,81 @@ def extract_sent(input_sent,position):
                 return sent,position
         else:
             start_i+=len(sent)
+
+class SentGenerator(defaultdict):
+    def __init__ (self,item_type,batchsize):
+        super(SentGenerator, self).__init__(item_type)
+        self.batchsize=batchsize
+        self.sent_ls=[]
+    def sent_generator(self,a_list):
+        
+            for i in a_list:
+                yield i
+                
+    def convert_to_generator(self,batchsize):
+        self.sent_ls=[]
+        for sent_l in self:
+            repeat=int(math.ceil(float(len(self[sent_l]))/self.batchsize))
+            self.sent_ls+=[sent_l]*repeat
+            self[sent_l]=self.sent_generator(self[sent_l])
+        np.random.seed(1034)
+        np.random.shuffle(self.sent_ls) 
+        
+def batch_data(line_list,batchsize):
+    sent_l2line_is=SentGenerator(list,batchsize)
+    for line_i,line in enumerate(line_list):
+#         print (list(line))
+        sent_l=len(list(line))
+        sent_l2line_is[sent_l].append(line_i)
+#     print (sent_l2line_is.keys())
+    sent_l2line_is.convert_to_generator(batchsize)
+    return sent_l2line_is
+
+
+def read_batch(f1, simp_lines,batchsize, word2index1):        
+    batch = []
+    line_inds=[]
+    while len(batch) < batchsize:
+        try:
+            line_i=next(f1)
+            line1 = simp_lines[line_i]
             
+        except StopIteration:
+            print ('{0} completed'.format(f1))
+            return batch,line_inds
+        if not line1: break
+        sent_words1 = list(line1)
+#         print (sent_words1)
+        sent_inds1 = []
+        for word in sent_words1:
+#             print (word)
+            word= word.encode('utf-8')
+            if word in word2index1:
+                
+                ind = word2index1[word]
+            else:
+                ind = word2index1['<UNK>']
+            sent_inds1.append(ind)
+        
+        batch.append(sent_inds1)
+        line_inds.append(line_i)
+    return batch,line_inds
+
+def next_batch(sent_l2line_is,simp_lines,batchsize):
+    
+    for sent_l in sent_l2line_is.sent_ls:
+        sent_arr,line_inds=read_batch(sent_l2line_is[sent_l],simp_lines,batchsize,mr.word2index1)
+        if sent_arr==[]:
+            continue
+        else:
+            yield xp.array(sent_arr),line_inds
+# sent_ys = self._contexts_rep(sent_arr)
+
+
 if __name__ == '__main__':
 
-    if len(sys.argv) < 4:
-        sys.stderr.write("Usage: %s <csv-filename> <conversion-table> <model-params-filename> [gpu]\n" % sys.argv[0])
+    if len(sys.argv) < 5:
+        sys.stderr.write("Usage: %s <csv-filename> <conversion-table> <model-params-filename> [gpu] [batchsize] \n" % sys.argv[0])
         sys.exit(1)
 
     csv_filename = sys.argv[1]
@@ -55,7 +126,9 @@ if __name__ == '__main__':
         xp = cp if gpu >= 0 else np
     mr = ModelReader(sys.argv[3],gpu)
 
-
+    
+    #batchsize
+    batchsize=int(sys.argv[5])
 
     # read conversion table
     trad2simp, var2norm = dict(), dict()
@@ -91,11 +164,14 @@ if __name__ == '__main__':
 
 
     # run conversion test
-    def predict(simp_sentence, pos):
-        simp_sentence,pos=extract_sent(simp_sentence,pos)
-        tokens = [t.encode('utf-8') for t in simp_sentence]
-        tokens = [t if t in mr.word2index1 else '<UNK>' for t in tokens]
-        context_embed = xp.array(mr.model.context2vec(tokens, pos))
+    def predict(simp_sentence, pos,contexts,line_current_i):
+#         simp_sentence,pos=extract_sent(simp_sentence,pos)
+#         tokens = [t.encode('utf-8') for t in simp_sentence]
+#         tokens = [t if t in mr.word2index1 else '<UNK>' for t in tokens]
+#        context_embed = xp.array(mr.model.context2vec(tokens, pos))
+#         print (simp_sentence.encode('utf-8'))
+#         print (contexts)
+        context_embed=contexts[pos].data[line_current_i]
         context_embed = context_embed / xp.sqrt((context_embed * context_embed).sum())
         simp = simp_sentence[pos]
         bestTrad, bestScore = None, None
@@ -111,18 +187,41 @@ if __name__ == '__main__':
     trad_error_count, trad_count = Counter(), Counter()
     error_list = pd.DataFrame(columns=['char_res', 'orig_char', 'gold_char', 'char_index',
                                        'res', 'orig', 'gold', 'orig_line_num'])
-    for i, row in csv.iterrows():
-        pos = row['char_index']
-        gold_char = normalize(row['gold_char'])
-        pred_char_raw = predict(row['orig'], pos)
-        pred_char = normalize(pred_char_raw)
-        trad_count[gold_char] += 1
-        print "%s%s" % (gold_char.encode('utf-8'), pred_char.encode('utf-8')),
-        if gold_char != pred_char:
-            orig_char = row['orig_char']
-            error_list.loc[len(error_list)] = [pred_char, orig_char, gold_char, pos,
-                                               pred_char_raw, row['orig'], row['gold'], row['orig_line_num']]
-            trad_error_count[gold_char] += 1
+    
+    #read in batches of sentences
+    batch_sent_generator=batch_data(csv['orig'],batchsize)
+#     print (batch_sent_generator)
+    for sent_arr,line_inds in next_batch(batch_sent_generator,csv['orig'],batchsize):
+#         print (sent_arr)
+#         print (line_inds)
+        mr.model.reset_state()
+        contexts=mr.model._contexts_rep(sent_arr) #a batch of context representations
+    
+        for line_current_i,line_i in enumerate(line_inds):
+    #     for i, row in csv.iterrows():
+#             print (line_i),
+            pos_lst = str(csv['char_index'][line_i]).split('-')
+            pos_lst=[p for p in pos_lst if p!='']
+            gold_chars=list(csv['gold_char'][line_i])
+            orig_chars=list(csv['orig_char'][line_i])
+            orig=csv['orig'][line_i]
+            orig_line_nums=[i for i in str(csv['orig_line_num'][line_i]).split('-') if i !='-']
+            
+            for i,pos in enumerate(pos_lst):
+                if pos=='':
+                    continue
+                pos=int(pos)
+                gold_char = normalize(gold_chars[i])
+#                 print (pos,line_current_i)
+                pred_char_raw = predict(orig,pos,contexts,line_current_i)
+                pred_char = normalize(pred_char_raw)
+                trad_count[gold_char] += 1
+                print "%s%s" % (gold_char.encode('utf-8'), pred_char.encode('utf-8')),
+                if gold_char != pred_char:
+                    orig_char = orig_chars[i]
+                    error_list.loc[len(error_list)] = [pred_char, orig_char, gold_char, pos,
+                                                       pred_char_raw, orig_chars[i], gold_chars[i], orig_line_nums[i]]
+                    trad_error_count[gold_char] += 1
     print
     error_list.to_csv(out_filename + '_c2v_errors.csv', index=False, encoding='utf-8')
 
